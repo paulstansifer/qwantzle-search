696274d63641166626e6679850b24fed27b21d8d
Paul Stansifer <paul.stansifer@gmail.com>
2024-09-17 17:49:06 +0000

    Reformat.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
- use std::io::Write;
+use std::io::Write;
 
 use llama_cpp::{standard_sampler::StandardSampler, LlamaModel, LlamaParams, SessionParams};
 
 fn main() {
     // Create a model from anything that implements `AsRef<Path>`:
 
-    let model = LlamaModel::load_from_file("/workspace/tiny_llama.gguf", LlamaParams::default()).expect("Could not load model");
+    let model = LlamaModel::load_from_file("/workspace/tiny_llama.gguf", LlamaParams::default())
+        .expect("Could not load model");
 
     // A `LlamaModel` holds the weights shared across many _sessions_; while your model may be
     // several gigabytes large, a session is typically a few dozen to a hundred megabytes!
-    let mut ctx = model.create_session(SessionParams::default()).expect("Failed to create session");
+    let mut ctx = model
+        .create_session(SessionParams::default())
+        .expect("Failed to create session");
 
     // You can feed anything that implements `AsRef<[u8]>` into the model's context.
-    ctx.advance_context("I am a DINOSAUR and I have some THOUGHTS about LARGE LANGUAGE MODELS.").unwrap();
+    ctx.advance_context("Once upon").unwrap();
 
     // LLMs are typically used to predict the next word in a sequence. Let's generate some tokens!
     let max_tokens = 1024;
@@ -20,7 +23,10 @@ fn main() {
 
     // `ctx.start_completing_with` creates a worker thread that generates tokens. When the completion
     // handle is dropped, tokens stop generating!
-    let mut completions = ctx.start_completing_with(StandardSampler::default(), 1024).unwrap().into_strings();
+    let mut completions = ctx
+        .start_completing_with(StandardSampler::default(), 1024)
+        .unwrap()
+        .into_strings();
 
     for completion in completions {
         print!("{completion}");
