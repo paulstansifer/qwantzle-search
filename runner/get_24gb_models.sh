a109ba4bbe568be2103e003f568f9008a796efc1
Paul Stansifer <paul.stansifer@gmail.com>
2024-09-29 20:01:21 +0000

    Update model planning,

--- runner/get_24gb_models.sh
+++ runner/get_24gb_models.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-cd /app/models
-huggingface-cli download mradermacher/gemma-2-27b-GGUF   gemma-2-27b-it.i1-Q4_0.gguf  --local-dir .
+cd /app/models/
+
 huggingface-cli download TheBloke/Mixtral-8x7B-v0.1-GGUF  mixtral-8x7b-v0.1.Q3_K_M.gguf  --local-dir .
 huggingface-cli download TheBloke/Yi-34B-GGUF  yi-34b.Q4_0.gguf  --local-dir .
-huggingface-cli download mradermacher/gemma-2-27b-GGUF  gemma-2-27b.Q4_K_S.gguf  --local-dir .
+# huggingface-cli download mradermacher/gemma-2-27b-GGUF  gemma-2-27b.Q4_K_S.gguf  --local-dir .
+huggingface-cli download mradermacher/Meta-Llama-3.1-70B-i1-GGUF Meta-Llama-3.1-70B.i1-IQ2_XS.gguf  --local-dir .
+huggingface-cli download mradermacher/Qwen2.5-32B-GGUF  Qwen2.5-32B.Q4_K_S.gguf  --local-dir .
--- runner/get_8gb_models.sh
+++ runner/get_8gb_models.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 cd /app/models/
+
 huggingface-cli download QuantFactory/TinyLlama_v1.1-GGUF  TinyLlama_v1.1.Q8_0.gguf  --local-dir .
 huggingface-cli download paul-stansifer/qw-us-mistral-7b-gguf  unsloth.Q4_K_M.gguf  --local-dir .
 mv unsloth.Q4_K_M.gguf qw-mistral.Q4_K_M.gguf
@@ -21,4 +22,6 @@ huggingface-cli download Aloshe/Sheared-LLaMA-1.3B-Q8_0-GGUF  sheared-llama-1.3b
 huggingface-cli download DevQuasar/Llama-3.2-3B-GGUF  Llama-3.2-3B.Q4_K_M.gguf --local-dir .
 huggingface-cli download QuantFactory/gemma-2-9b-GGUF   gemma-2-9b.Q4_K_S.gguf --local-dir .
 huggingface-cli download Aryanne/Sheared-LLaMA-2.7B-gguf  q4_0-sheared-llama-2.7b.gguf   --local-dir .
- huggingface-cli download TheBloke/Mistral-7B-OpenOrca-GGUF  mistral-7b-openorca.Q4_0.gguf --local-dir .
+huggingface-cli download TheBloke/Mistral-7B-OpenOrca-GGUF  mistral-7b-openorca.Q4_0.gguf --local-dir .
+huggingface-cli download paul-stansifer/qw-mistral-7b-gguf  unsloth.Q4_K_M.gguf  --local-dir .
+mv unsloth.Q4_K_M.gguf qw-nonus-mistral.Q4_K_M.gguf
