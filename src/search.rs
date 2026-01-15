828c80173fc09d4abbb945335f059b36600b653d
Paul Stansifer <paul.stansifer@gmail.com>
2026-01-15 13:42:16 -0500

    Let's set the RLNN schedule to the one that seemed a bit better.

--- src/search.rs
+++ src/search.rs
@@ -862,10 +862,9 @@ pub fn prob_score(
     }
 
     let rlnn_sched = vec![
-        (0, 3.0),
-        (len.saturating_sub(40), 2.5),
+        (0, 2.0),
+        (len.saturating_sub(40), 2.0),
         (len.saturating_sub(20), 1.5),
-        (len.saturating_sub(10), 0.0),
         (len, 0.0),
     ];
     let rlnn_mult = (rlnn_prob + 0.5).powf(ilerp(chars_so_far, &rlnn_sched));
